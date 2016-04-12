73cd4a1acebd5c2c0760d15716b87bafe0efd219
dcnh35 <dcnh35@yahoo.com>
2016-04-12 17:41:35 +0800

    first commit: annotation

---
+++ src/annotation/PasswordUtils.java
@@ -0,0 +1,22 @@
+package annotation;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/4/12.
+ */
+public class PasswordUtils {
+    @UseCase(id=47,description = "Passwirds must contain at least one password")
+    public boolean validatePassword(String password) {
+        return password.matches("\\w*\\d\\w*");
+    }
+
+    @UseCase(id=48)
+    public String encryptPassword(String password) {
+        return new StringBuilder(password).reverse().toString();
+    }
+
+    public boolean checkForNewPassword(List<String> prevPasswords,String password) {
+        return !prevPasswords.contains(password);
+    }
+}
